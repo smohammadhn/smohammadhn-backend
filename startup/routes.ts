b0985297b3de5aee2dc8b1f61942fcdb9d3658a2
smohammadhn <smohammadhnd@gmail.com>
2023-12-05 20:51:54 +0200

    chore(routes.ts): remove commented code

--- startup/routes.ts
+++ startup/routes.ts
@@ -9,30 +9,3 @@ export default async function (app: Application) {
   // global error handler (works in combination with express-async-errors)
   app.use(error)
 }
-
-// export default async function (app: Application) {
-//   let routeFileNames: string[] = ['home']
-
-//   // read all files inside /router directory and register them as routes
-//   // avoid manually creating route middleware everytime a new route is created!
-//   await fs.promises
-//     .readdir(config.get('path') + '/routes/')
-//     .then((files) => {
-//       routeFileNames = [...files]
-//     })
-//     .catch((err) => logger.error(err.message, err))
-
-//   routeFileNames.forEach((routeFileName) => {
-//     const routeName = path.parse(routeFileName).name
-
-//     const routeMiddlewarePath = require(`../routes/${routeFileName}`)
-
-//     let endPoint = `/api/${routeName}`
-//     if (routeName === 'home') endPoint = '/'
-
-//     app.use(endPoint, routeMiddlewarePath)
-//   })
-
-//   // global error handler (works in combination with express-async-errors)
-//   app.use(error)
-// }
